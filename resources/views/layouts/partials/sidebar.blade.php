2bc8c73b46b2d619f996936385adbd09c93aedfa
WillianAbrego <willianantonioabrego@gmail.com>
2024-01-01 13:30:05 -0600

    view project

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -28,7 +28,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link ">
+              <a href="{{ route('admin.projects.index') }}" class="nav-link ">
                 <i class="nav-icon fas fa-project-diagram"></i>
                 <p>Proyectos</p>
               </a>
